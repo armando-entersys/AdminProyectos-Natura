a3435d5dbd50d9b139b475624a775499233fc547
ajcor <ajcortest@gmail.com>
2024-08-23 20:34:51 -0600

    Estructura principal

---
+++ BusinessLayer/Concrete/UsuarioManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    internal class UsuarioManager : IUsuarioService
+    {
+        private readonly IUsuarioDal _usuarioDal;
+        public UsuarioManager(IUsuarioDal usuarioDal)
+        {
+            _usuarioDal = usuarioDal;
+        }
+
+        public void TDelete(int id)
+        {
+            _usuarioDal.Delete(id);
+        }
+
+        public List<Usuario> TGetAll()
+        {
+           return  _usuarioDal.GetAll();
+        }
+
+        public Usuario TGetById(int id)
+        {
+           return _usuarioDal.GetById(id);
+        }
+
+        public void TInsert(Usuario entity)
+        {
+            _usuarioDal.Insert(entity);
+        }
+
+        public void TUpdate(Usuario entity)
+        {
+            _usuarioDal.Update(entity);
+        }
+    }
+}
---
+++ DataAccessLayer/Context/DataAccesContext.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Context
+{
+    public class DataAccesContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=solucionesmkt.com.mx;initial catalog=AdminProyectosNaturaDB;user id=mkt;password=123456789;");
+
+        }
+        public DbSet<TipoUsuario> TiposUsuario { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Alerta> Alertas { get; set; }
+        public DbSet<Brief> Briefs { get; set; }
+        public DbSet<TipoBrief> TiposBrief { get; set; }
+        public DbSet<EstatusBrief> EstatusBriefs { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+
+
+    }
+}
